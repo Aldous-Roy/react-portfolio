f550a6091a9ae817a4d52d471dccfa87530e040d
Aldous Roy <aldousroy007@gmail.com>
2024-12-05 22:48:07 +0530

    linkedin issue resolved

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -9,28 +9,28 @@ const Navbar = () => {
         <img className="mx-2 w-14" src={logo} alt="Logo" />
       </div>
       <div className="m-8 flex items-center justify-center gap-4 text-2xl">
-        <a 
-          href="www.linkedin.com/in/aldousroy2004" 
-          target="_blank" 
-          rel="noopener noreferrer" 
+        <a
+          href="https://www.linkedin.com/in/aldousroy2004"
+          target="_blank"
+          rel="noopener noreferrer"
           aria-label="LinkedIn"
           className="text-blue-600 hover:text-blue-800"
         >
           <FaLinkedin />
         </a>
-        <a 
-          href="https://github.com/Aldous-Roy" 
-          target="_blank" 
-          rel="noopener noreferrer" 
+        <a
+          href="https://github.com/Aldous-Roy"
+          target="_blank"
+          rel="noopener noreferrer"
           aria-label="GitHub"
           className="text-black hover:text-gray-800"
         >
           <FaGithub />
         </a>
-        <a 
-          href="https://www.instagram.com/aldous.roy/" 
-          target="_blank" 
-          rel="noopener noreferrer" 
+        <a
+          href="https://www.instagram.com/aldous.roy/"
+          target="_blank"
+          rel="noopener noreferrer"
           aria-label="Instagram"
           className="text-pink-500 hover:text-pink-700"
         >
