8e77cfaac0dd7978475eb0196a5bd5a896d29aef
Aldous Roy <aldousroy007@gmail.com>
2024-12-05 16:57:39 +0530

    hero section modified

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,8 +1,8 @@
 import React from 'react'
 const About = () => {
   return (
-    <div>
-
+    <div className='border-b border-neutral-900 pb-4'>
+      <h2 className='my-20 text-center text-4xl'>About Me</h2>
     </div>
   )
 }
