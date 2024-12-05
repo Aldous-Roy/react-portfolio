31624dfd54e24a36a48fdbd82a0f8a23f2b8a69f
Aldous Roy <aldousroy007@gmail.com>
2024-12-05 16:51:16 +0530

    updates

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
+import About from "./components/About";
 
 const App = () => {
   return (
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -13,7 +13,7 @@ const Hero = () => {
             <span className="bg-gradient-to-r from-pink-300 via-slate-500 to-purple-500 bg-clip-text text-4xl tracking-tighter font-bold text-transparent">
               Full Stack Developer
             </span>
-            <p className="my-2 max-w-xl py-6 font-light tracking-tighter text-justify">
+            <p className="my-2 text-xl max-w-xl py-6 font-light tracking-tighter text-justify">
               {HERO_CONTENT}
             </p>
           </div>
